25a4627d3acaa5eb4503d23f9817b294f186a0d1
Arthur Molnar <arthur486@gmail.com>
2025-09-30 00:45:47 +0300

    Starter code for first seminar added

---
+++ src/seminar/group322/Seminar1.java
@@ -0,0 +1,18 @@
+package seminar.group322;
+
+/*
+    Se lucreaza cu o lista de numere. Aceste numere se pot citi in doua moduri:
+        - din linia de comanda
+        - daca nu exista argumente in linia de comanda, de la tastatura
+    In orice mod ar fi citita lista, este necesara gestionarea cazului in care se citeste si altceva decat un numar.
+
+    Scrieti o aplicatie cu un meniu care are urmatoarele functionalitati:
+        1. Afisati minimul din lista de numere.
+        2. Eliminati numerele prime din lista.
+        3. Cautare numar dat de la tastatura
+        3. Gasiti cea mai lunga secventa de numere impare din lista.
+        4. Iesire
+*/
+
+public class Seminar1 {
+}
