a615b403c7c385b3ae4598027951e53af1e772b1
arthur <arthur@cs.ubbcluj.ro>
2025-09-30 19:31:33 +0300

    am avansat putin ...

--- src/seminar/group322/Seminar1.java
+++ src/seminar/group322/Seminar1.java
@@ -14,14 +14,85 @@ package seminar.group322;
         4. Iesire
 */
 
-public class Seminar1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println(args.length);
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
-        // enhanced for loop
+public class Seminar1 {
+    private static List<Integer> parseArguments(String[] args) {
+        List<Integer> numbers = new ArrayList<Integer>(); // lista stocata pe un vector
+//        List<Integer> numbers2 = new LinkedList<>();  // lista dublu inlantuita
+
         for (String arg : args) {
-            System.out.println(arg);
+            try {
+                numbers.add(Integer.parseInt(arg));
+            } catch (NumberFormatException e) {
+
+            }
         }
+
+        return numbers;
+    }
+
+    private static void printMenu() {
+        System.out.println("1. Afisati minimul din lista de numere");
+        System.out.println("2. Eliminati numerele prime din lista");
+        System.out.println("3. Cautare numar dat de la tastatura");
+        System.out.println("4. Gasiti cea mai lunga secventa de numere impare din lista");
+        System.out.println("5. Afiseaza toate numerele din lista");
+        System.out.println("0. Iesire");
+    }
+
+    private static void startUI(List<Integer> numbers) {
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            printMenu();
+            String line = scanner.nextLine();
+
+            switch (line) {
+                case "1":
+                    continue;
+                case "2":
+                    continue;
+                case "3":
+                    continue;
+                case "4":
+                    continue;
+                case "5":
+                    System.out.println("Lista de numere:");
+                    for (Integer num : numbers) {
+                        System.out.print(num + " ");
+                    }
+                    System.out.println();
+                case "0":
+                    return;
+                default:
+                    System.out.println("Optiunea nu exista!");
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+//        int x = 7;
+//        List<Integer> numbers = new LinkedList<>();
+//        numbers.add(x);
+
+        if (args.length == 0) {
+            System.out.println("Introduceti numerele separate prin spatiu: ");
+            Scanner sc = new Scanner(System.in);
+            String inputLine = sc.nextLine();
+            String[] inputArray = inputLine.split(" ");
+            args = inputArray;
+        }
+
+        startUI(parseArguments(args));
+//        for (Integer arg : numbers) {
+//            System.out.println(arg);
+//        }
     }
 }
